cb51aea69a4b6d4ee44cc89234a5b6529c8a162f
Shabda Raaj <shabda@agiliq.com>
2010-09-24 16:13:49 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "django-buzz",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Agiliq and friends",
+    author_email ="shabda@agiliq.com", 
+    description = "Django app to find What is hot and buzzing in a topic.",
+    url = "http://github.com/agiliq/django-buzz",
+    include_package_data = True
+)
