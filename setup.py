2af8ab0c0883967607c7a2766a3fab2d1621e05c
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-10-17 17:19:56 +0530

    Bumped the version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "django-buzz",
-    version = "0.1.1",
+    version = "0.1.2",
     packages = find_packages(),
     author = "Agiliq and friends",
     author_email ="shabda@agiliq.com", 
