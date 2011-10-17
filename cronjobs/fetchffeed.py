aa65443864f7600e40de2c5471dfdd621b0d50a8
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-10-17 17:28:12 +0530

    Some more code cleansing.

--- cronjobs/fetchffeed.py
+++
@@ -1,8 +0,0 @@
-#con job for fetching the friend feed for every 10 minutes
-
-#! /usr/bin/env python
-
-from doloto.pystories.services.EntryService import FetchManager
-
-fmanager = FetchManager()
-fmanager.triggerfetchFeed()
